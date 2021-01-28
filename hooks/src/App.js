a2809d7b2ed8a880c1a9dd5bb9b032bbb63e8289
ning1315 <59202849+ning1315@users.noreply.github.com>
2021-01-28 17:16:23 +0900

    useTitle

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import React,{ useState } from 'react';
 import BasicuseState from './component/BasicuseState'
+import UseTitle from './component/useTitle'
 
 const App = () => {
   return (
     <div className="App">
-     <BasicuseState/>
+     <UseTitle/>
     </div>
   );
 }
